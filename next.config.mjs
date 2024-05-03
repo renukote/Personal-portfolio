0bf083deb27662368293c8e863a86607a30f4d08
Renuka Prasad Kote <renukaprasad.kb@gmail.com>
2024-05-04 01:17:52 +0530

    init: initiate the project

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "user-images.githubusercontent.com",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
---
+++ tailwind.config.ts
@@ -0,0 +1,55 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: "#823ae0",
+        primary50: "#823ae080",
+        primary80: "#823ae0cc",
+        primary400: "#9456e5",
+        primary600: "#762ad9",
+        secondary: "#212121",
+        tertiary: "#eaeaea",
+        tertiary80: "#eaeaeacc",
+      },
+      fontFamily: {
+        primary: ["var(--font-poppins)"],
+        secondary: ["var(--font-secondary)"],
+      },
+      screens: {
+        sm: "640px",
+        md: "800px",
+        lg: "1100px",
+        xl: "1300px",
+        "2xl": "1536px",
+      },
+      keyframes: {
+        appear: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(0px, 100px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate(0px, 0px)",
+          },
+        },
+      },
+      animation: {
+        appear: "appear 0.7s ease-in-out",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
